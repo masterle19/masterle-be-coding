a98fd4e71681a84f8eaa5f96cf559fc1fb64dd25
Master Le <iam@mast3rle.xyz>
2024-01-09 13:53:37 -0600

    cha

--- studio/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'u62jjaxi',
-    dataset: 'production'
-  }
-})
--- studio/schemas/index.js
+++
@@ -1,4 +0,0 @@
-import blog from './blog'
-import category from './category.js'
-
-export const schemaTypes = [blog, category]
